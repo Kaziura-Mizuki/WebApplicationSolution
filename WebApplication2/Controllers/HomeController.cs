990e0eb642f224ff7271fea345dcd88cd0038a02
Katayama-git <tkm_katayama@1excellence.com>
2025-03-03 09:28:52 +0900

    修正

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Collections.Generic;
 using WebApplication2.Models;
-using static WebApplication2.Models.ResultViewModel;
 using Microsoft.Extensions.Logging;
 using static WebApplication2.Models.AppDbContext;
 using Microsoft.EntityFrameworkCore;
@@ -36,63 +35,6 @@ namespace WebApplication2.Controllers
             return Json(new { message = "Greetings from server!" });
         }
 
-        public ActionResult GetData(int id)
-        {
-            var resultlist = new List<Result>();
-            var result = new Result();
-            var model = new ResultViewModel();
-            result.FirstName = "";
-            result.FamilyName = "";
-            if(id > 0 || id <= model.familyNameList.Length)
-            {
-                result.FamilyName = model.familyNameList[id];
-                result.FirstName = model.firstNameList[id];
-            }
-            resultlist.Add(result);
-            return Json(new { data = resultlist });
-        }
 
-        public ActionResult GetTodo()
-        {
-            var resultlist = new List<TodoViewModel>();
-            var result = new TodoViewModel();
-            var data = _context.Todo.ToList();
-            if (data.Count == 0)
-            {
-                result.user_id = 0;
-                result.username = "No data found";
-                result.password = "No data found";
-                resultlist.Add(result);
-            }
-            else
-            {
-                for (int i = 0; i < data.Count; i++)
-                {
-                    result.user_id = data[i].user_id;
-                    result.username = data[i].username;
-                    result.password = data[i].password;
-                    resultlist.Add(result);
-                }
-            }
-            return Json(new { data = resultlist });
-        }
-
-        public ActionResult CreateUser(TodoViewModel model)
-        {
-            if (model == null)
-            {
-                return Json(new { status = false });
-            }
-            else
-            {
-                Todo todo = new Todo();
-                var newId = commonService.GetNextSequenceValue("Users_seq");
-                todo.user_id = newId;
-                todo.username = model.username;
-                todo.password = model.password;
-                _context.Todo.Add(todo);
-                return Json(new { status = true });
-            }
-        }
     }
 }
--- WebApplication2/Models/AppDbContext.cs
+++ WebApplication2/Models/AppDbContext.cs
@@ -7,7 +7,6 @@ using System.Linq;
 
 namespace WebApplication2.Models
 {
-    //using Microsoft.EntityFrameworkCore;
 
     public class AppDbContext: DbContext
     {
@@ -15,16 +14,12 @@ namespace WebApplication2.Models
         {
         }
 
-        //public DbSet<ErrorViewModel> ErrorViewModels { get; set; }
 
-        public virtual DbSet<Todo> Todo{ get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Todo>().ToTable("Users");
-            modelBuilder.Entity<Todo>().HasKey(p => p.user_id);
         }
     }
 }
